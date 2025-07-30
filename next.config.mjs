e726be3b75a6ffb77e8bb6844e5381495d82f057
Junaid-Ahmad <junaidsandhu21@gmail.com>
2025-07-30 16:35:36 +0500

    setup the project and install packages

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    plugins: {
+        '@tailwindcss/postcss': {},
+        autoprefixer: {},
+    }
+};
 
 export default nextConfig;
